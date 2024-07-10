f4d1a54392171ec5914b6a1d30a0a9ce90340f19
lizhihong <zhihong.li@petkit.com>
2024-07-10 18:24:35 +0800

    feat(init): first

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,14 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn greet1(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet,greet1])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
